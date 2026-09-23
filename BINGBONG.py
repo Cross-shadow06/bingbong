2c0bd4a6c3acb305c33e438ba342ad5bff2c37b3
Cross-shadow06 <artiartiya@list.ru>
2026-09-23 22:50:05 +0400

    not not
    v0.0.0.3

--- BINGBONG.py
+++ BINGBONG.py
@@ -23,22 +23,34 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
-    def update(self):
+    def update_l(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
 
 
-plauer1 = Player('slon.png', 50, 400, 80, 100, 15)
-plauer2 = Player('bron.png', 350, 400, 80, 100, 15)
-bill = GameSprite('oler.png', 200, 200, 10, 20, 10)
+plauer1 = Player('slon.png', 50, 400, 90, 110, 15)
+plauer2 = Player('bron.png', 550, 400, 90, 110, 15)
+bill = GameSprite('oler.png', 200, 200, 40, 40, 10)
 
 font.init()
 font1 = font.SysFont('Arial', 60)
-win = font1.render('YOUS WIN!!!', True, (255, 215, 0))
-lose = font1.render('YOUS deid hahahaha', True, (177,22,22))
+win_l = font1.render('YOUSl WIN!!!', True, (255, 215, 0))
+win_r = font1.render('YOUSr WIN!!!', True, (155, 115, 0))
+lose_l = font1.render('YOUSl deid hahahaha', True, (77,222,22))
+lose_r = font1.render('YOUSr deid hahahaha', True, (177,22,22))
+
+speed_x = 3
+speed_y = 3
 
 run = True
 finish = False
@@ -50,12 +62,24 @@ while run:
 
     if not finish:
         window.blit(background, (0, 0))
-        plauer1.update()
+        bill.rect.x += speed_x
+        bill.rect.y += speed_y
+        if bill.rect.y > win_height-50 or bill.rect.y <0:
+            speed_y *= -1
+        if sprite.collide_rect(plauer1, bill) or sprite.collide_rect(plauer2, bill):
+            speed_x *= -1
+        if bill.rect.x < 0:
+            finish = True
+            window.blit(lose_l, (202, 202))
+        if bill.rect.x > win_width:
+            finish = True
+            window.blit(lose_r, (202, 202))
+        plauer1.update_l()
         plauer1.reset()
-        plauer2.update()
+        plauer2.update_r()
         plauer2.reset()
-        bill.update()
         bill.reset()
+
 
     display.update()
     clock.tick(FPS)
